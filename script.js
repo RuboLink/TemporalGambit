9fb97ce6acdb0f4a5730331e6a7b961726c74562
Rubén Martínez Sánchez <ruboms@gmail.com>
2026-06-02 17:04:21 +0200

    Get the chosen time

--- script.js
+++ script.js
@@ -2,32 +2,27 @@ const addIncrementBox = document.getElementById("increment_box");
 const incrementList = document.getElementById("increment_list");
 const differentBox = document.getElementById("different_box");
 const differentTimes = document.getElementById("different_times");
-const whiteTime = document.getElementById("white_time")
-const body = document.getElementsByTagName("body");
-const tiemposList = `<select id="tiempos_list">
-                    <option>1 min</option>
-                    <option>3 min</option>
-                    <option>5 min</option>
-                    <option>10 min</option>
-                    <option>15 min</option>
-                    <option>20 min</option>
-                    <option>30 min</option>
-                    <option>60 min</option>
-                    <option>90 min</option>
-                </select>`;
+const main = document.getElementsByTagName("main");
+const readyButton = document.getElementById("ready_button")
+const tiemposList = `<select class="tiempos_list">
+                        <option value="1">1 min</option>
+                        <option value="3">3 min</option>
+                        <option value="5">5 min</option>
+                        <option value="10">10 min</option>
+                        <option value="15">15 min</option>
+                        <option value="20">20 min</option>
+                        <option value="30">30 min</option>
+                        <option value="60">60 min</option>
+                        <option value="90">90 min</option>
+                    </select>`;
 
 const incrementosList = `<select id="incrementos_list">
-                        <option>+1 seg</option>
-                        <option>+3 seg</option>
-                        <option>+5 seg</option>
-                        <option>+10 seg</option>
-                     </select>`;
+                        <option value="1">+1 seg</option>
+                        <option value="3">+3 seg</option>
+                        <option value="5">+5 seg</option>
+                        <option value="10">+10 seg</option>
+                    </select>`;
 
-
-
-async function firstLoad(){
-    differentTimes.innerHTML = `<label id="white_time">Tiempo de cada reloj: ${tiemposList}</label>`;
-}
 
 addIncrementBox.addEventListener("click", async () => {
     if(addIncrementBox.checked){
@@ -38,13 +33,30 @@ addIncrementBox.addEventListener("click", async () => {
 
 differentBox.addEventListener("click", async () => {
     if(differentBox.checked){
-        differentTimes.innerHTML = `<label>Tiempo de piezas blancas: ${tiemposList}</label>
+        differentTimes.innerHTML = `<label id="white_time">Tiempo de piezas blancas: ${tiemposList}</label>
                                     <br/>
-                                    <label>Tiempo de piezas negras: ${tiemposList}</label>
+                                    <label id="black_time">Tiempo de piezas negras: ${tiemposList}</label>
                                     <br/>`
     } else { 
         differentTimes.innerHTML = `<label>Tiempo de cada reloj: ${tiemposList}</label>`;
     }
 })
 
+readyButton.addEventListener("click", async () => {
+    readyButton.hidden = true;
+    if(differentBox.checked){
+        let whiteLabel = document.getElementById("white_time");
+        let whiteSelect = whiteLabel.querySelector("select");
+        
+        let blackLabel = document.getElementById("black_time");
+        let blackSelect = blackLabel.querySelector("select");
+        
+    } else {
+        let timeLabel = document.getElementById("both_times");
+        let times = timeLabel.querySelector("select");
 
+        
+    }
+
+    readyButton.hidden = false;
+})
